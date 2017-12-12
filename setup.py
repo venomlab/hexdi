c8cb7340bec3f5b7e942ef6a91621c08ed6fcfcb
dimonzo <zibertscrem@gmail.com>
2017-12-12 02:53:06 +0200

    Updated version to 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='hexdi',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(include=('hexdi',)),
     classifiers=[
         # How mature is this project? Common values are
