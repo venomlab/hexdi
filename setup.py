2363ab79380d61580e99d26a26efc538360564df
dmitriy <dmitriy.selischev@logicify.com>
2018-01-16 19:54:29 +0200

    Bumped version to 0.1.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='hexdi',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(include=('hexdi',)),
     classifiers=[
         # How mature is this project? Common values are
